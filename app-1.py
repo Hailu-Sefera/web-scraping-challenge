7857c12c1dbf6e51abff02c6c7fd67b67194cabf
Hailu-Sefera <50960138+Hailu-Sefera@users.noreply.github.com>
2019-10-18 13:55:27 -0500

    Add files via upload

---
+++ app-1.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+# Import scrape_mars1
+import scrape_mars3
+
+# Import our pymongo library, which lets me connect my Flask app to my Mongo database.
+import pymongo
+
+# Create an instance of my Flask app.
+app = Flask(__name__)
+
+# Create connection variable
+# conn = "mongodb://localhost:27017/scrape_mars1_app"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/scrape_mars1_app"
+mongo = PyMongo(app)
+
+
+# Pass connection to the pymongo instance.
+# client = pymongo.MongoClient(conn)
+# db = client.mars_data1
+# collection = db.mars_data1
+
+# Set route
+@app.route("/")
+def index1():
+    # mars = client.db.mars.find_one()
+    mars = mongo.db.mars.find_one()
+    return render_template("index1.html", mars=mars)
+
+# Scrape 
+@app.route("/scrape")
+def scrape():
+    # mars = client.db.mars
+    mars1 = mongo.db.mars1 
+    mars_data1 = scrape_mars3.scrape()
+    print(mars_data1)
+    mars1.update({}, mars_data1,upsert=True )
+    return redirect("http://localhost:5000/", code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ scrape_mars3.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Setup
+
+# In[74]:
+
+
+
+# Dependencies
+from splinter import Browser
+from bs4 import BeautifulSoup
+import pandas as pd
+from pprint import pprint
+#import time
+#from selenium import webdriver
+import time
+
+def scrape():
+    executable_path = {'executable_path': 'chromedriver.exe'}
+    browser = Browser('chrome', **executable_path, headless=False)
+
+    url = 'https://mars.nasa.gov/news/'
+    browser.visit(url)
+    time.sleep(2)
+    html = browser.html
+    mars_news_soup = BeautifulSoup(html, 'html.parser')
+    first_title = mars_news_soup.find('div', class_='content_title').text
+    first_paragraph = mars_news_soup.find('div', class_='article_teaser_body').text
+
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(url)
+    time.sleep(2)
+    browser.click_link_by_partial_text('FULL IMAGE')
+    time.sleep(2)
+    browser.click_link_by_partial_text('more info')
+    html = browser.html
+    image_soup = BeautifulSoup(html, 'html.parser')
+    feat_img_url = image_soup.find('figure', class_='lede').a['href']
+    feat_img_full_url = f'https://www.jpl.nasa.gov{feat_img_url}'
+    feat_img_full_url
+
+    url = 'https://twitter.com/marswxreport?lang=en'
+    browser.visit(url)
+    time.sleep(2)
+    html = browser.html
+    tweet_soup = BeautifulSoup(html, 'html.parser')
+    first_tweet = tweet_soup.find('p', class_='TweetTextSize').text
+    url = 'https://space-facts.com/mars/'
+    tables = pd.read_html(url)
+    df = tables[0]
+    df.to_html()
+
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url)
+    time.sleep(2)
+    html = browser.html
+    hemi_soup = BeautifulSoup(html, 'html.parser')
+    hemi_strings = []
+    links = hemi_soup.find_all('h3')
+
+    for hemi in links:
+        hemi_strings.append(hemi.text)
+        
+    hemispheres_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(hemispheres_url)
+    time.sleep(2)
+    html = browser.html
+    soup = BeautifulSoup(html, "html.parser")
+    mars_hemisphere = []
+
+    products = soup.find("div", class_ = "result-list" )
+    hemispheres = products.find_all("div", class_="item")
+
+    for hemisphere in hemispheres:
+        title = hemisphere.find("h3").text
+        title = title.replace("Enhanced", "")
+        end_link = hemisphere.find("a")["href"]
+        image_link = "https://astrogeology.usgs.gov/" + end_link    
+        browser.visit(image_link)
+        time.sleep(2)
+        html = browser.html
+        soup=BeautifulSoup(html, "html.parser")
+        downloads = soup.find("div", class_="downloads")
+        image_url = downloads.find("a")["href"]
+        mars_hemisphere.append({"title": title, "img_url": image_url})
+
+    mars_hemisphere
+
+    browser.quit()
+
+    return mars_hemisphere
